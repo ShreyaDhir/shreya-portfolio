967e9f48374540792b8ec1d5cad0758878a3beed
ShreyaDhir <dhirshreya@gmail.com>
2022-01-24 13:46:45 +0530

    dark mode toggle

--- libs/theme.js
+++ libs/theme.js
@@ -16,8 +16,44 @@ const components = {
     Heading: {
         variants: {
             'section-title': {
-                textDecoration: ''
+                textDecoration: 'underline',
+                fontSize: 20,
+                textUnderlineOffset: 6,
+                textDecorationColor: '#525252',
+                textDecorationThickness: 4,
+                marginTop: 3,
+                marginBottom: 4
             }
         }
+    },
+    Link: {
+        baseStyle: props => ({
+            color: module('#3d7aed', '#ff63c3')(props),
+            textUnderlinedOffset: 3
+        })
     }
 }
+
+const fonts = {
+    heading: " 'M PLUS Rounded 1c' ",
+}
+
+const color = {
+    glassTeal: '#88ccca',
+}
+
+const config = {
+    initialColorMode: 'dark',
+    useSystemColorMode: true
+}
+
+const theme = extendTheme({
+    config,
+    style,
+    components,
+    color,
+    fonts
+})
+
+export default theme;
+
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,10 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import Layout from '../components/layouts/main';
+import theme from '../libs/theme';
 
-const Website = ({ Component, pageProps, router }) => {
+const Website = ({ Component, pageProps, router, theme }) => {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Layout router={router}>
         <Component {...pageProps} key={router.router} />
       </Layout>
